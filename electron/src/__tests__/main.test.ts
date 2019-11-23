afd31062788b59edb71be5c63e4e599de92b0502
Yuichi Yogo <yuichkun@gmail.com>
2019-11-23 17:08:12 +0900

    chore: add test for app.initialize

--- electron/src/__tests__/main.test.ts
+++ electron/src/__tests__/main.test.ts
@@ -1,7 +1,30 @@
 import App from '../main'
 
 describe('App', () => {
-  it('app is defined', () => {
-    expect(App).toBeDefined()
+  let app: App;
+
+  beforeEach(() => {
+    app = new App();
+  })
+
+  describe('constructor', () => {
+    it('app is defined', () => {
+      expect(App).toBeDefined()
+      expect(app).toBeInstanceOf(App)
+    })
+  })
+
+  describe('initialize', () => {
+    it('loads models', async () => {
+      expect(app.models).toBeUndefined();
+      await app.initialize()
+      expect(app.models).toBeDefined();
+    })
+    it('sets loop id at 0', async () => {
+      app.currentLoopId = 100;
+      expect(app.currentLoopId).toBe(100)
+      await app.initialize()
+      expect(app.currentLoopId).toBe(0)
+    })
   })
 })
